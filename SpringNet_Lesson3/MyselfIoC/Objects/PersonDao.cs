d14d15ecb3b8ff1bb749d5916b6ffc9ab77c80dd
t430 <evaseemefly@126.com>
2016-09-18 20:28:26 +0800

    9月18日 第一次创建

---
+++ SpringNet_Lesson3/MyselfIoC/Objects/PersonDao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyselfIoC
+{
+    public class PersonDao
+    {
+        public override string ToString()
+        {
+            return "我是PersonDao";
+        }
+    }
+}
---
+++ SpringNet_Lesson3/MyselfIoC/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyselfIoC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppRegistry();
+            Console.ReadLine();
+        }
+
+        static void AppRegistry()
+        {
+            MyXmlFactory ctx = new MyXmlFactory(@"D:\Objects.xml");
+            Console.WriteLine(ctx.GetObject("PersonDao").ToString());
+        }
+    }
+}
